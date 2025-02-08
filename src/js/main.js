5169419c6599d233ccacefb4d063a232e0f60310
ISAAC <85229264+PashaBritva@users.noreply.github.com>
2025-02-08 13:56:34 +0300

    Revert "Added"

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,3 @@ function RegistrationFrame() {
     const centerDiv = document.getElementById("about-b")
     centerDiv.style.backgroundColor = "#f5f5f5"
 }
-
-function setBackElement() {
-    const currentHash = window.location.hash;
-
-    const links = document.querySelectorAll(".section-li");
-    links.forEach(link => link.classList.remove("active-gray"));
-
-    switch (currentHash) {
-        case "#about":
-            document.getElementById("about-link").classList.add("active-gray");
-            break;
-        case "#info":
-            document.getElementById("info-link").classList.add("active-gray");
-            break;
-        case "#news":
-            document.getElementById("news-link").classList.add("active-gray");
-            break;
-        default:
-            break;
-    }
-}
-
-window.addEventListener("load", setBackElement);
-window.addEventListener("hashchange", setBackElement);
